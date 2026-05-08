883cc816d5894cfc68067d4b4e802a533caf18bd
RukerDevps <gamingrook271@gmail.com>
2026-05-08 10:29:02 +0530

    Add create_super.py script and update requirements

---
+++ create_super.py
@@ -0,0 +1,14 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HMS_project.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'arunakhil978@gmail.com', '12345678')
+    print('Superuser created successfully')
+else:
+    print('Superuser already exists')
